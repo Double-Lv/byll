b7797b6899bfee60d59787edadff67abaf5c7681
Double-Lv <lvxiaobao_fbi@163.com>
2014-06-09 17:43:11 +0800

    add file

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+
+<title>不要乱来</title>
+<link rel="stylesheet" type="text/css" href="css/main.css">
+<script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
+<script src="js/html2canvas.js"></script>
+<script src="js/canvas2image.js"></script>
+<script src="js/base64.js"></script>
+</head>
+<body>
+<div class="container">
+	<header>
+		<h1>不要</h1>
+		<h1>乱来</h1>
+	</header>
+	<section>
+		<div id="byll_1">
+			<h2>第1期</h2>
+			<div class="ctlpanel">
+				<div class="paneltitle">操作区</div>
+				<a class="insertbtn" href="javascript:void(0);">插入字幕</a>
+				<div class="targetarea">
+					<img class="resourceimg" src="php/resource/demo.jpg" />
+				</div>
+				<a class="generatebtn" href="javascript:void(0);">生成图片</a>
+			</div>
+			<div class="previewpanel">
+				<div class="paneltitle">预览区</div>
+				<img class="previewimg" src="" />
+			</div>
+			<div id="byll_img_1" class="convertpanel"></div>
+		</div>
+		
+	</section>
+</div>
+
+<script type="text/javascript" src="js/main.js"></script>	
+<script type="text/javascript" charset="utf-8">
+
+</script>
+</body>
+</html>
