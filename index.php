b6bd1e027c63f182284d868028f14540b1aa2889
Double-Lv <lvxiaobao_fbi@163.com>
2014-06-14 18:59:25 +0800

    add file

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 
 <title>不要乱来</title>
 <link rel="stylesheet" type="text/css" href="css/main.css">
-<script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
+<script src="js/jquery-2.1.1.min.js"></script>
+<script src="js/dragresize_commented.js"></script>
 <script src="js/html2canvas.js"></script>
 <script src="js/canvas2image.js"></script>
 <script src="js/base64.js"></script>
@@ -13,21 +14,47 @@
 <body>
 <div class="container">
 	<header>
-		<h1>不要</h1>
-		<h1>乱来</h1>
+		<!-- <h1>不要</h1>
+		<h1>乱来</h1> -->
 	</header>
 	<section>
 		<div id="byll_1" class="phase">
-			<h2>第1期</h2>
+			<!-- <h2>第1期</h2> -->
 			<div class="ctlpanel">
 				<div class="paneltitle">
 					<span>操作区</span>
-					<a class="insertbtn" href="javascript:void(0);">插入字幕</a>
+					<a class="insertbublebtn" href="javascript:void(0);">插入文字</a>
 					<a class="generatebtn" href="javascript:void(0);">生成图片</a>
 				</div>
 				
 				<div class="targetarea">
-					<img class="resourceimg" src="php/resource/demo.jpg" />
+					<img class="resourceimg" src="img/512.png" />
+				</div>
+				<div class="coverpanel">
+					<div id="byll_img_1" class="convertpanel"></div>
+				</div>
+				
+			</div>
+			<div class="previewpanel">
+				<div class="paneltitle">预览区</div>
+				<img class="previewimg" src="" />
+				<div class="downloadpanel">
+					图片已生成，你可以<a class="downbtn large button green" href="javascript:void(0);">点此下载</a>,或者<a class="sharetoweibo large button red" href="javascript:void(0);">分享到微博</a>
+				</div>
+			</div>
+		</div>
+
+		<div id="byll_20140611" class="phase">
+			<!-- <h2>第1期</h2> -->
+			<div class="ctlpanel">
+				<div class="paneltitle">
+					<span>操作区</span>
+					<a class="insertbtn" href="javascript:void(0);">插入文字</a>
+					<a class="generatebtn" href="javascript:void(0);">生成图片</a>
+				</div>
+				
+				<div class="targetarea">
+					<img class="resourceimg" src="php/resource/20140611.jpg" />
 				</div>
 				<div class="coverpanel">
 					<div id="byll_img_1" class="convertpanel"></div>
